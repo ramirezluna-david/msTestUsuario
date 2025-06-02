88eec21bb675d77cc5fe516ec757f97dc0fdd02a
David A. Ramírez Luna <davidrrluna@gmail.com>
2025-06-02 01:35:47 -0400

    commit

--- src/main/java/com/edutech/msTestUsuario/controller/UserController.java
+++ src/main/java/com/edutech/msTestUsuario/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,6 +40,16 @@ public class UserController {
             return new ResponseEntity<>(nuevoUsuario, HttpStatus.ACCEPTED);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+        }
+    }
+
+    @GetMapping("/{idUser}")
+    public ResponseEntity<User> getUser(@PathVariable int idUser) {
+        User buscarUsuario = userService.findById((idUser));
+        if(buscarUsuario != null) {
+            return new ResponseEntity<>(buscarUsuario, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 }
